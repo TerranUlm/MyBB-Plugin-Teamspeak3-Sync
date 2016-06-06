c0f9c70d96c5db8145f3a00feeed164aaaca102c
Dieter Gobbers <dieter@gobbers.de>
2016-06-06 14:18:09 +0200

    updated for MyBB 1.8.x
    some small adjustments to make it compatible with MyBB 1.8.x

--- inc/plugins/teamspeak3.php
+++ inc/plugins/teamspeak3.php
@@ -4,28 +4,18 @@
 Teamspeak 3 Group Sync Plugin for MyBB
 Copyright (C) 2013 Dieter Gobbers
 
-The MIT License (MIT)
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-Copyright (c) 2016 Dieter Gobbers
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 if(!defined('IN_MYBB'))
@@ -42,9 +32,9 @@ function teamspeak3_info()
 		'description'=>'Syncs forum and Teamspeak3 groups.',
 		'website'=>'http://opt-community.de/',
 		'author'=>'Dieter Gobbers',
-		'version'=>'1.0.0',
+		'version'=>'1.1.0',
 		'guid'=>'',
-		'compatibility'=>'16*',
+		'compatibility'=>'18*',
 		'codename'=>'teamspeak3'
 	);
 }
@@ -247,13 +237,7 @@ function teamspeak3_admin_user_groups_edit_graph_tabs_1(&$tabs)
 {
 	global $lang;
 	$lang->load('teamspeak3');
-	$tabs = array(
-                "general" => $lang->general,
-                "forums_posts" => $lang->forums_posts,
-                "users_permissions" => $lang->users_permissions,
-                "misc" => $lang->misc,
-                "ts" => $lang->ts3
-        );
+	$tabs["ts"] = $lang->ts3;
 	return $tabs;
 }
 
