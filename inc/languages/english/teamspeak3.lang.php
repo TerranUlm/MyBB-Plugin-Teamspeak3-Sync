0921e6643d1b9a87bcf840a42d3472dd754e4c30
Dieter Gobbers <dieter@gobbers.de>
2016-06-20 13:50:07 +0200

    Update teamspeak3.lang.php

--- inc/languages/english/teamspeak3.lang.php
+++ inc/languages/english/teamspeak3.lang.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Teamspeak 3 Group Sync Plugin for MyBB
+TeamSpeak 3 Group Sync Plugin for MyBB
 Copyright (C) 2013 Dieter Gobbers
 
 This program is free software: you can redistribute it and/or modify
@@ -17,27 +17,30 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-$l['ts3'] = "Teamspeak 3";
-$l['ts3_server_address'] = "Teamspeak 3 Server address";
-$l['ts3_server_address_description'] = "Set the Teamspeak 3 Server IP or hostname.";
-$l['ts3_server_port'] = "Teamspeak 3 Server Query Port";
-$l['ts3_server_port_description'] = "Set the Teamspeak 3 Server Query Port.";
-$l['ts3_username'] = "Teamspeak 3 Server Query User";
-$l['ts3_username_description'] = "Set the Teamspeak 3 Server Query User.";
-$l['ts3_password'] = "Teamspeak 3 Server Query User Password";
-$l['ts3_password_description'] = "Set the Teamspeak 3 Server Query User Password.";
-$l['ts3_vserverport'] = "Teamspeak 3 Virutal Server Port";
-$l['ts3_vserverport_description'] = "Set the Teamspeak 3 Virtual Server Port.";
-$l['ts3_guest_channel_group'] = "Teamspeak 3 Server Guest Channel Group ID";
+$l['ts3'] = "TeamSpeak 3";
+$l['ts3_server_address'] = "TeamSpeak 3 Server address";
+$l['ts3_server_address_description'] = "Set the TeamSpeak 3 Server IP or hostname.";
+$l['ts3_server_port'] = "TeamSpeak 3 Server Query Port";
+$l['ts3_server_port_description'] = "Set the TeamSpeak 3 Server Query Port.";
+$l['ts3_username'] = "TeamSpeak 3 Server Query User";
+$l['ts3_username_description'] = "Set the TeamSpeak 3 Server Query User.";
+$l['ts3_password'] = "TeamSpeak 3 Server Query User Password";
+$l['ts3_password_description'] = "Set the TeamSpeak 3 Server Query User Password.";
+$l['ts3_vserverport'] = "TeamSpeak 3 Virutal Server Port";
+$l['ts3_vserverport_description'] = "Set the TeamSpeak 3 Virtual Server Port.";
+$l['ts3_guest_channel_group'] = "TeamSpeak 3 Server Guest Channel Group ID";
 $l['ts3_guest_channel_group_description'] = "This will be the default group for channels controlled by this plugin.";
-$l['ts3_sgid'] = "Teamspeak 3 Server Group ID";
+$l['ts3_sgid'] = "TeamSpeak 3 Server Group ID";
 $l['ts3_sgid_description'] = "Database ID of the Server Group.";
-$l['ts3_cgid'] = "Teamspeak 3 Channel Group ID";
+$l['ts3_cgid'] = "TeamSpeak 3 Channel Group ID";
 $l['ts3_cgid_description'] = "Primary channel for this group, subchannels inherit the Channel Group automatically unless you assign another group to them.";
-$l['ts3_cid'] = "Teamspeak 3 Channel ID";
+$l['ts3_cid'] = "TeamSpeak 3 Channel ID";
 $l['ts3_cid_description'] = "Database ID of the Channel.";
 $l['ts3_sort'] = "Sorting sequence";
 $l['ts3_sort_description'] = "highest number wins as a user can be member of one channel group per channel only.";
-$l['ts3_data'] = "Teamspeak3 Channel Data";
+$l['ts3_data'] = "TeamSpeak3 Channel Data";
 $l['ts3_task_description'] = "automatically fix permissions";
+$l['ts3_framework_path'] = "TeamSpeak 3 PHP Framework Path";
+$l['ts3_framework_path_description'] = "where to find the teamspeak3.php file of the TeamSpeak 3 PHP Framework";
+
 ?>
